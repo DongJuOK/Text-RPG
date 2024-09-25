b94a9ab5f40d5e891f45ae845ce71f8b2e86e0f9
DongJuOK <kcary0907@gmail.com>
2024-09-25 21:47:20 +0900

    (2024.9.25) 3차 작업

---
+++ Program/game.h
@@ -0,0 +1,16 @@
+#include "main.h"
+
+#pragma once
+
+#define HEIGHT 9
+#define WIDTH 38
+
+#define UP 'w'
+#define DOWN 's'
+#define LEFT 'a'
+#define RIGHT 'd'
+#define SUBMIT ' '
+
+void title();
+int menu();
+void info();
---
+++ Program/util.c
@@ -0,0 +1,21 @@
+#include "util.h"
+
+void setSystem()
+{
+	system("mode con cols=76 lines=26");
+
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO ConsoleCursor;
+	ConsoleCursor.bVisible = 0;
+	ConsoleCursor.dwSize = 1;
+	SetConsoleCursorInfo(consoleHandle, &ConsoleCursor);
+}
+
+void gotoxy(int x, int y)
+{
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD pos;
+	pos.X = x;
+	pos.Y = y;
+	SetConsoleCursorPosition(consoleHandle, pos);
+}
